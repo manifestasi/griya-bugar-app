ead1429157a99c81057a7caa1415e31f079c927f
colondum <decarabian21@gmail.com>
2024-12-15 22:36:33 +0700

    update: upload data profile and photo using cloudinary

--- app/src/main/java/com/griya/griyabugar/data/respository/UserDataRepository.kt
+++ app/src/main/java/com/griya/griyabugar/data/respository/UserDataRepository.kt
@@ -1,12 +1,23 @@
 package com.griya.griyabugar.data.respository
 
+import android.content.Context
+import android.net.Uri
 import android.util.Log
+import com.cloudinary.android.MediaManager
+import com.cloudinary.android.callback.ErrorInfo
+import com.cloudinary.android.callback.UploadCallback
+import com.cloudinary.android.policy.UploadPolicy
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.griya.griyabugar.data.Resource
+import com.griya.griyabugar.data.UploadResult
 import com.griya.griyabugar.data.model.DataUser
+import com.griya.griyabugar.util.ImageProcess
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.tasks.await
@@ -15,8 +26,137 @@ import javax.inject.Inject
 
 class UserDataRepository @Inject constructor (
     private val firestore: FirebaseFirestore,
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val mediaManager: MediaManager,
+    @ApplicationContext private val context: Context
 ) {
+
+    fun uploadDataProfile(
+        uri: Uri?,
+        email: String,
+        noTelepon: String,
+        nama: String,
+        kelamin: String
+    ): Flow<UploadResult<String>> = callbackFlow {
+
+        trySend(UploadResult.Loading)
+
+        val uuidUser = firebaseAuth.currentUser?.uid ?: ""
+
+        if (uri != null){
+            val realPath = ImageProcess.getRealPathFromUri(context, uri)
+            val compressImage = ImageProcess.compressImage(realPath)
+
+            val requestId = mediaManager.upload(compressImage.absolutePath)
+                .option("public_id", "folder_profile/${uuidUser}")
+                .option("overwrite", true)
+                .option("resource_type", "image")
+                .policy(UploadPolicy.defaultPolicy())
+                .callback(object : UploadCallback{
+                    override fun onStart(requestId: String?) {
+                        trySend(UploadResult.Loading)
+                    }
+
+                    override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
+                        val progress = (bytes.toDouble() / totalBytes) * 100
+                        trySend(UploadResult.Progress(progress.toInt()))
+                    }
+
+                    override fun onSuccess(
+                        requestId: String?,
+                        resultData: MutableMap<Any?, Any?>?
+                    ) {
+                        val url = resultData?.get("secure_url").toString()
+
+
+                        firestore.collection(AuthRepository.COLLECTION_USER)
+                            .document(uuidUser)
+                            .get()
+                            .addOnSuccessListener {
+
+                                val data = it.data
+                                firestore.collection(AuthRepository.COLLECTION_USER)
+                                    .document(uuidUser)
+                                    .set(
+                                        DataUser(
+                                            nama = nama,
+                                            noTelepon = noTelepon,
+                                            email = email,
+                                            role = data?.get("role").toString(),
+                                            kelamin = kelamin,
+                                            foto = url
+                                        )
+                                    )
+                                    .addOnSuccessListener {
+                                        trySend(UploadResult.Success(
+                                            "Update profile successfull"
+                                        )).isSuccess
+                                        close() // Menutup flow setelah sukses
+                                    }
+                                    .addOnFailureListener {
+                                        trySend(UploadResult.Error(it.message.toString()))
+                                        close()
+                                    }
+
+                            }
+                            .addOnFailureListener {
+                                trySend(UploadResult.Error(it.message.toString()))
+                                close()
+                            }
+
+
+                    }
+
+                    override fun onError(requestId: String?, error: ErrorInfo?) {
+                        trySend(UploadResult.Error(error?.description ?: "Unknown error"))
+                        close()
+                    }
+
+                    override fun onReschedule(requestId: String?, error: ErrorInfo?) {
+                        trySend(UploadResult.Error("Upload rescheduled"))
+                    }
+
+                }).dispatch()
+
+            // Pembersihan ketika flow selesai atau dibatalkan
+            awaitClose { mediaManager.cancelRequest(requestId) }
+        } else {
+            firestore.collection(AuthRepository.COLLECTION_USER)
+                .document(uuidUser)
+                .get()
+                .addOnSuccessListener {
+
+                    val data = it.data
+                    firestore.collection(AuthRepository.COLLECTION_USER)
+                        .document(uuidUser)
+                        .set(
+                            DataUser(
+                                nama = nama,
+                                noTelepon = noTelepon,
+                                email = email,
+                                role = data?.get("role").toString(),
+                                kelamin = kelamin,
+                                foto = data?.get("foto").toString()
+                            )
+                        )
+                        .addOnSuccessListener {
+                            trySend(UploadResult.Success(
+                                "Update profile successfull"
+                            )).isSuccess
+                            close() // Menutup flow setelah sukses
+                        }
+                        .addOnFailureListener {
+                            trySend(UploadResult.Error(it.message.toString()))
+                            close()
+                        }
+
+                }
+                .addOnFailureListener {
+                    trySend(UploadResult.Error(it.message.toString()))
+                    close()
+                }
+        }
+    }
 
     fun getDataProfile(): Flow<Resource<DataUser?>> = flow {
         emit(Resource.Loading)
--- app/src/main/java/com/griya/griyabugar/ui/screen/main/myaccount/editprofile/EditProfileScreen.kt
+++ app/src/main/java/com/griya/griyabugar/ui/screen/main/myaccount/editprofile/EditProfileScreen.kt
@@ -1,6 +1,7 @@
 package com.griya.griyabugar.ui.screen.main.myaccount.editprofile
 
 import android.net.Uri
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Canvas
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,12 +37,16 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.griya.griyabugar.data.Resource
+import com.griya.griyabugar.data.UploadResult
 import com.griya.griyabugar.ui.components.Field.DropDownField
 import com.griya.griyabugar.ui.components.appbar.AppBarWithBackButton
+import com.griya.griyabugar.ui.components.dialog.ErrorDialog
+import com.griya.griyabugar.ui.components.loading.LoadingAnimation
 import com.griya.griyabugar.ui.components.profile.image.CircleImageProfile
 import com.griya.griyabugar.ui.components.register.ButtonConfirm
 import com.griya.griyabugar.ui.components.register.NoHandPhoneField
 import com.griya.griyabugar.ui.components.register.TextField
+import com.griya.griyabugar.ui.navigation.Screen
 import com.griya.griyabugar.ui.theme.Gray
 import com.griya.griyabugar.ui.theme.GriyaBugarTheme
 import com.griya.griyabugar.ui.theme.poppins
@@ -50,6 +56,8 @@ fun EditProfileScreen(
     rootNavControll: NavHostController = rememberNavController(),
     editProfileViewModel: EditProfileViewModel = hiltViewModel()
 ){
+    /* context from activity */
+    val context = LocalContext.current
 
     var expanded by rememberSaveable { mutableStateOf(false) }
     var selectedGender by rememberSaveable { mutableStateOf("") }
@@ -60,12 +68,20 @@ fun EditProfileScreen(
     var isDisabled by rememberSaveable { mutableStateOf(true) }
 
     var imageUrl by rememberSaveable { mutableStateOf<String>("") }
+    var imageUri by rememberSaveable { mutableStateOf<Uri?>(null) }
+
+    var isLoading by rememberSaveable { mutableStateOf(false) }
+
+    var isLoadingUpload by rememberSaveable { mutableStateOf(false) }
+    var isErrorUpload by rememberSaveable { mutableStateOf(false) }
+    var errorMessage by rememberSaveable { mutableStateOf("") }
 
     // Membuat launcher untuk membuka galeri
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
     ) { uri: Uri? ->
         imageUrl = uri.toString() // Simpan URI gambar yang dipilih
+        imageUri = uri
     }
 
     if(
@@ -86,72 +102,175 @@ fun EditProfileScreen(
         editProfileViewModel.dataProfile.collect { event ->
             when (event){
                 is Resource.Loading -> {
-
+                    isLoading = true
                 }
                 is Resource.Success -> {
-
+                    isLoading = false
+                    val data = event.data
+                    data?.let {
+                        nama = it.nama ?: ""
+                        email = it.email ?: ""
+                        noTelepon = it.noTelepon ?: ""
+                        selectedGender = it.kelamin ?: ""
+                        imageUrl = it.foto ?: ""
+                    }
                 }
                 is Resource.Error -> {
-
+                    isLoading = false
+                    Toast.makeText(context, event.errorMessage, Toast.LENGTH_LONG).show()
                 }
                 is Resource.Empty -> {
-
+                    isLoading = false
                 }
+            }
+        }
+
+        editProfileViewModel.uploadDataProfileEvent.collect { event ->
+            when (event){
+                is UploadResult.Loading -> {
+                    isLoadingUpload = true
+                }
+                is UploadResult.Success -> {
+                    isLoadingUpload = false
+                    Toast.makeText(
+                        context,
+                        event.data,
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    rootNavControll.navigate(
+                        Screen.Main.route
+                    ){
+                        popUpTo(0){
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+                is UploadResult.Error -> {
+                    isLoadingUpload = false
+                    isErrorUpload = true
+                    errorMessage = event.errorMessage
+                }
+                is UploadResult.Reschedule -> {
+                    isLoadingUpload = false
+                    isErrorUpload = true
+                    errorMessage = "Rescheduled upload"
+                }
+                else ->{}
             }
         }
     }
 
-    Scaffold(
-        topBar = {
-            AppBarWithBackButton(
-                title = "Edit Profile",
-                onClickBack = {
-                    rootNavControll.popBackStack()
-                }
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .verticalScroll(rememberScrollState())
-                .padding(horizontal = 16.dp)
-        ) {
+    if (isErrorUpload){
+        ErrorDialog(
+            onDismiss = {
 
-            Spacer(Modifier.height(32.dp))
+            },
+            title = "Oops..",
+            description = errorMessage,
+            buttonText = "Oke",
+            buttonOnClick = {
+                isErrorUpload = false
+            }
+        )
+    }
 
+    /* Loading */
+    if(isLoading){
+        LoadingAnimation()
+    } else {
+        Scaffold(
+            topBar = {
+                AppBarWithBackButton(
+                    title = "Edit Profile",
+                    onClickBack = {
+                        rootNavControll.popBackStack()
+                    }
+                )
+            }
+        ) { innerPadding ->
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState())
+                    .padding(horizontal = 16.dp)
             ) {
-                Box(modifier = Modifier.clickable { launcher.launch("image/*") }) {
-                    CircleImageProfile(
-                        size = 96,
-                        url = imageUrl
-                    )
 
-                    Canvas(
-                        modifier = Modifier
-                            .size(24.dp)
-                            .align(Alignment.BottomEnd)
-                            .offset(
-                                x = (-4).dp,
-                            )
-                    ){
-                        drawCircle(
-                            color = Gray,
-                            radius = size.minDimension / 2
+                Spacer(Modifier.height(32.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box(modifier = Modifier.clickable { launcher.launch("image/*") }) {
+                        CircleImageProfile(
+                            size = 96,
+                            url = imageUrl
                         )
+
+                        Canvas(
+                            modifier = Modifier
+                                .size(24.dp)
+                                .align(Alignment.BottomEnd)
+                                .offset(
+                                    x = (-4).dp,
+                                )
+                        ){
+                            drawCircle(
+                                color = Gray,
+                                radius = size.minDimension / 2
+                            )
+                        }
                     }
                 }
-            }
 
-            Spacer(Modifier.height(32.dp))
+                Spacer(Modifier.height(32.dp))
 
-            Column {
+                Column {
+                    Text(
+                        text = "Nama",
+                        style = TextStyle(
+                            fontFamily = poppins,
+                            fontWeight = FontWeight(400),
+                            fontSize = 20.sp
+                        )
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    TextField(
+                        onChange = {
+                            nama = it
+                        },
+                        value = nama,
+                        placeHolder = ""
+                    )
+                }
+
+                Spacer(Modifier.height(18.dp))
+
+                Column {
+                    Text(
+                        text = "Nomor Telepon",
+                        style = TextStyle(
+                            fontFamily = poppins,
+                            fontWeight = FontWeight(400),
+                            fontSize = 20.sp
+                        )
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    NoHandPhoneField(
+                        onChange = {
+                            noTelepon = it
+                        },
+                        value = noTelepon
+                    )
+                }
+
+                Spacer(Modifier.height(18.dp))
+
                 Text(
-                    text = "Nama",
+                    text = "Email",
                     style = TextStyle(
                         fontFamily = poppins,
                         fontWeight = FontWeight(400),
@@ -161,18 +280,17 @@ fun EditProfileScreen(
                 Spacer(Modifier.height(8.dp))
                 TextField(
                     onChange = {
-                        nama = it
+                        email = it
                     },
-                    value = nama,
-                    placeHolder = ""
+                    placeHolder = "Masukkan Email",
+                    value = email,
+                    readOnly = true
                 )
-            }
 
-            Spacer(Modifier.height(18.dp))
+                Spacer(Modifier.height(18.dp))
 
-            Column {
                 Text(
-                    text = "Nomor Telepon",
+                    text = "Jenis Kelamin",
                     style = TextStyle(
                         fontFamily = poppins,
                         fontWeight = FontWeight(400),
@@ -180,64 +298,36 @@ fun EditProfileScreen(
                     )
                 )
                 Spacer(Modifier.height(8.dp))
-                NoHandPhoneField(
-                    onChange = {
-                        noTelepon = it
+                DropDownField(
+                    label = "Pilih jenis kelamin",
+                    selectedValue = selectedGender,
+                    expanded = expanded,
+                    onExpandedChange = {
+                        expanded = !it
                     },
-                    value = noTelepon
+                    options = genderOptions,
+                    onSelected = {
+                        selectedGender = it
+                    }
+                )
+
+                Spacer(Modifier.height(36.dp))
+
+                ButtonConfirm(
+                    onClick = {
+                        editProfileViewModel.uploadDataProfile(
+                            uri = imageUri,
+                            email = email,
+                            noTelepon = noTelepon,
+                            nama = nama,
+                            kelamin = selectedGender
+                        )
+                    },
+                    name = "Simpan",
+                    isDisabled = isDisabled || isLoadingUpload,
+                    isLoading = isLoadingUpload
                 )
             }
-
-            Spacer(Modifier.height(18.dp))
-
-            Text(
-                text = "Email",
-                style = TextStyle(
-                    fontFamily = poppins,
-                    fontWeight = FontWeight(400),
-                    fontSize = 20.sp
-                )
-            )
-            Spacer(Modifier.height(8.dp))
-            TextField(
-                onChange = {
-                    email = it
-                },
-                placeHolder = "Masukkan Email",
-                value = email
-            )
-
-            Spacer(Modifier.height(18.dp))
-
-            Text(
-                text = "Jenis Kelamin",
-                style = TextStyle(
-                    fontFamily = poppins,
-                    fontWeight = FontWeight(400),
-                    fontSize = 20.sp
-                )
-            )
-            Spacer(Modifier.height(8.dp))
-            DropDownField(
-                label = "Pilih jenis kelamin",
-                selectedValue = selectedGender,
-                expanded = expanded,
-                onExpandedChange = {
-                    expanded = !it
-                },
-                options = genderOptions,
-                onSelected = {
-                    selectedGender = it
-                }
-            )
-
-            Spacer(Modifier.height(36.dp))
-
-            ButtonConfirm(
-                onClick = {},
-                name = "Simpan",
-                isDisabled = isDisabled
-            )
         }
     }
 
