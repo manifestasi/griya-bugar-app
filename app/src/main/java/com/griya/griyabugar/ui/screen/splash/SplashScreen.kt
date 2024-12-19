cf68f0a0fcee4e63d561fdac1e1bc5f9590788fa
colondum <decarabian21@gmail.com>
2024-12-19 21:34:01 +0700

    update tampilan splash screen

--- app/src/main/java/com/griya/griyabugar/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/griya/griyabugar/ui/screen/splash/SplashScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,7 +19,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.griya.griyabugar.R
+import com.griya.griyabugar.ui.components.CircleElemen.CircleElement2
 import com.griya.griyabugar.ui.components.statusbar.UpdateStatusBarColor
+import com.griya.griyabugar.ui.theme.GreenColor3
 import com.griya.griyabugar.ui.theme.GreenColor5
 import com.griya.griyabugar.ui.theme.GriyaBugarTheme
 import kotlinx.coroutines.delay
@@ -26,21 +30,21 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     onNavigateToWelcome: () -> Unit,
     onNavigateToMain: () -> Unit,
-    splashViewModel: SplashViewModel = hiltViewModel()
+//    splashViewModel: SplashViewModel = hiltViewModel()
 ){
 
     UpdateStatusBarColor(
         darkIcons = false
     )
 
-    LaunchedEffect(true) {
-        delay(2000)
-        if(splashViewModel.getCurrentUser() !== null){
-            onNavigateToMain()
-        } else {
-            onNavigateToWelcome()
-        }
-    }
+//    LaunchedEffect(true) {
+//        delay(2000)
+//        if(splashViewModel.getCurrentUser() !== null){
+//            onNavigateToMain()
+//        } else {
+//            onNavigateToWelcome()
+//        }
+//    }
 
     Box(
         modifier = Modifier
@@ -48,6 +52,41 @@ fun SplashScreen(
             .background(color = GreenColor5),
         contentAlignment = Alignment.Center
     ){
+        CircleElement2(
+            modifier = Modifier
+                .size(200.dp)
+                .align(Alignment.TopEnd)
+                .offset(x = (100).dp, y = (-100).dp),
+            colors = listOf(
+                GreenColor3,
+                GreenColor3
+            )
+        )
+
+        CircleElement2(
+            modifier = Modifier
+                .size(258.dp),
+            startAngle = 0f,
+            endAngle = 360f,
+            colors = listOf(
+                GreenColor3,
+                GreenColor3
+            )
+        )
+
+        CircleElement2(
+            modifier = Modifier
+                .size(118.dp)
+                .align(Alignment.BottomEnd)
+                .offset(x = (-34).dp, y = (-120).dp),
+            startAngle = 0f,
+            endAngle = 360f,
+            colors = listOf(
+                GreenColor3,
+                GreenColor3
+            )
+        )
+
         Image(
             painter = painterResource(R.drawable.logo),
             contentDescription = "Logo",
